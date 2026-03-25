83d3c4eaad91a705e67d9c3c93f5d1856b1052c6
TonyGil-CMinds <tony@cminds.co>
2026-03-25 01:11:09 -0600

    my 3d element

--- src/components/SplineBackground.jsx
+++ src/components/SplineBackground.jsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 export default function SplineBackground({ scene }) {
   // We provide a fallback Spline URL so you can see it working immediately.
   // Replace this URL with your custom exported Spline scene URL.
-  const defaultScene = "https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode";
+  const defaultScene = "https://prod.spline.design/JhX5K9Ysh75aJq8A/scene.splinecode";
 
   return (
     <div className="spline-background" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
